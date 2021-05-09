efe7370671353b4cb37d64b72d814410bb1c1a96
qdwang <wangqidong@gmail.com>
2021-05-09 11:39:35 +0800

    build native libs

--- Library/QRPBridge/build.linux.sh
+++ Library/QRPBridge/build.linux.sh
@@ -1,1 +1,1 @@
-g++ -fpic -shared -flinker-output=dyn -O3 -o ../../GUI/Asset/Lib/Bin/QRPBridge.so -I./godot-headers -I../LibRaw main.cpp ../LibRaw/bin/libraw_mod.linux.a
+g++ -fpic -shared -O3 -o ../../GUI/Asset/Lib/Bin/QRPBridge.so -I./godot-headers -I../LibRaw main.cpp ../LibRaw/bin/libraw_mod.linux.a
