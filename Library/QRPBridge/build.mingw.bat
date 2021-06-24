66aca8741105614cc866ba2430541b67d75acbd8
qdwang <wangqidong@gmail.com>
2021-06-24 15:49:14 +0800

    adjust build flags

--- Library/QRPBridge/build.mingw.bat
+++ Library/QRPBridge/build.mingw.bat
@@ -1,1 +1,7 @@
-g++ main.cpp ../LibRaw/lib/libraw_mod.mingw.a ../libjpeg-turbo/lib/libturbojpeg.mingw.a -o ../../GUI/Asset/Lib/Bin/QRPBridge.dll -D_WINDLL -static -lz -lWs2_32 -shared -Ofast -flto -funroll-loops -I./godot-headers -I../LibRaw -I../libjpeg-turbo 
+g++ main.cpp ../LibRaw/lib/libraw_mod.mingw.a ../libjpeg-turbo/lib/libturbojpeg.mingw.a ^
+-o ../../GUI/Asset/Lib/Bin/QRPBridge.dll ^
+-D_WINDLL ^
+-Wall -pedantic -Wextra -Wno-unused-parameter ^
+-static -shared -lWs2_32 -lz ^
+-Ofast -funroll-loops -flto ^
+-I./godot-headers -I../LibRaw -I../libjpeg-turbo 
