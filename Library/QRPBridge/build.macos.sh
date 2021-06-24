652f9958f323c2557a38da0199cb7e2270794d8f
qdwang <wangqidong@gmail.com>
2021-06-24 17:23:11 +0800

    udpate build script for macos

--- Library/QRPBridge/build.macos.sh
+++ Library/QRPBridge/build.macos.sh
@@ -1,1 +1,7 @@
-clang++ -arch x86_64 -arch arm64 -dynamiclib -O3 -o ../../GUI/Asset/Lib/Bin/QRPBridge.dylib -lz -I./godot-headers -I../LibRaw -I../libjpeg-turbo main.cpp ../LibRaw/lib/libraw_mod.macos.a ../libjpeg-turbo/lib/libturbojpeg.macos.a
+clang++ main.cpp ../LibRaw/lib/libraw_mod.linux.a ../libjpeg-turbo/lib/libturbojpeg.linux.a \
+-o ../../GUI/Asset/Lib/Bin/QRPBridge.dylib \
+-Wall -pedantic -Wextra -Wno-unused-parameter \
+-arch x86_64 -arch arm64 \
+-dynamiclib -lz \
+-Ofast -funroll-loops \
+-I./godot-headers -I../LibRaw -I../libjpeg-turbo 
