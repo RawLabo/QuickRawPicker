6d54d1b0a3f68dccb54b6071e4660ca843c06c80
qdwang <wangqidong@gmail.com>
2021-07-04 19:40:34 +0800

    adding detect range for leftpanel in main

--- GUI/Scene/Main.gd
+++ GUI/Scene/Main.gd
@@ -13,7 +13,7 @@ func show_fullscreen_photo(photo_frame):
   
 func _input(event):
   if event is InputEventMouseMotion:
-    if event.position.x < 20 and $LeftPanel.rect_position.x <= -200:
+    if event.position.x < 100 and $LeftPanel.rect_position.x <= -200:
       $LeftPanel/LeftPanelAni.play("Slide")
     elif event.position.x > 400 and $LeftPanel.rect_position.x == 0 and $Grid.get_child_count() > 0:
       $LeftPanel/LeftPanelAni.play_backwards("Slide")
