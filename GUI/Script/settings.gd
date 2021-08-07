dc90c82e6637225912df3bc35a43ac4a0b3e25b9
qdwang <wangqidong@gmail.com>
2021-08-07 11:53:22 +0800

    bump version to v0.1.11

--- GUI/Script/settings.gd
+++ GUI/Script/settings.gd
@@ -15,7 +15,7 @@ var export_folder = ""
 
 var select_color = Color(1, 1, 1)
 var mark_color = Color(0.5, 1, 0.3)
-var version = "v0.1.10"
+var version = "v0.1.11"
 
 onready var project_name = ProjectSettings.get_setting("application/config/name")
 
