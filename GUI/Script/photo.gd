1e4927325f14cc85b3bdd0274a282b6877318af9
qdwang <wangqidong@gmail.com>
2021-08-14 22:16:58 +0800

    fix list shifting

--- GUI/Script/photo.gd
+++ GUI/Script/photo.gd
@@ -205,7 +205,7 @@ func get_datetime():
   return "%d-%02d-%02d %02d:%02d" % [time["year"], time["month"], time["day"], time["hour"], time["minute"]]
 
 func get_list_info():
-  return "%d x %d\nF%.1f   %ss   ISO%1.f\n%.1fmm\n%s %s%s\n%s\n%s" % [
+  return " %d x %d\nF%.1f   %ss   ISO%1.f\n%.1fmm\n%s %s%s\n%s\n%s" % [
     width, height,
     aperture,
     shutter_speed_str,
