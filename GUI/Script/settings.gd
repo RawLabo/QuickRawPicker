815018d6e68be7d7f674944e247fd050984e67fd
qdwang <wangqidong@gmail.com>
2021-08-20 21:59:05 +0800

    bump version to v0.1.13

--- GUI/Script/settings.gd
+++ GUI/Script/settings.gd
@@ -33,7 +33,7 @@ onready var language = get_fixed_locale()
 
 var select_color = Color(1, 1, 1)
 var mark_color = Color(0.5, 1, 0.3)
-var version = "v0.1.12"
+var version = "v0.1.13"
 
 onready var project_name = ProjectSettings.get_setting("application/config/name")
 
