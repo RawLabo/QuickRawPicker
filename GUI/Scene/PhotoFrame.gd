a92cfc939b19241140c42234b20dd974f7efdae2
qdwang <wangqidong@gmail.com>
2021-08-20 13:31:14 +0800

    another scale method fix

--- GUI/Scene/PhotoFrame.gd
+++ GUI/Scene/PhotoFrame.gd
@@ -153,14 +153,11 @@ func rescale(is_scale_up, index = -1, reposition_center = false):
   var factor = scale_options[scale_index]
   
   $Photo.scale = Vector2(factor, factor)
-  if prev_scale_index == 0 and scale_index == 1:
-    if Settings.zoom_at_af_point and $Photo/FocusPos.visible:
-      reposition((1 if $Photo.rotation_degrees == 180 else -1) * $Photo/FocusPos.position)
-    else:
-      var pos = rect_min_size / 2 if reposition_center else Util.Nodes["Grid"].cursor_pos_in_frame
-      reposition(($Photo.position - pos) * (factor / pre_factor) + rect_min_size / 2 - $Photo.position)
+  if Settings.zoom_at_af_point and $Photo/FocusPos.visible and prev_scale_index == 0 and scale_index == 1:
+    reposition((1 if $Photo.rotation_degrees == 180 else -1) * $Photo/FocusPos.position)
   else:
-    reposition(($Photo.position - rect_min_size / 2) * (factor / pre_factor - 1))
+    var pos = rect_min_size / 2 if reposition_center else Util.Nodes["Grid"].cursor_pos_in_frame
+    reposition(($Photo.position - pos) * (factor / pre_factor) - ($Photo.position - pos))
   
 
 func reposition(pos):
