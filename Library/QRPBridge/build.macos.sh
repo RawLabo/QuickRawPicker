5a23e06c7b202b88f1ccf68d52e2f1ed0aad4ff4
qdwang <wangqidong@gmail.com>
2021-09-16 14:56:46 +0800

    add -mmacosx-version-min to macos build script

--- Library/QRPBridge/build.macos.sh
+++ Library/QRPBridge/build.macos.sh
@@ -2,6 +2,7 @@ clang++ main.cpp ../LibRaw/lib/libraw_mod.macos.a ../libjpeg-turbo/lib/libturboj
 -o ../../GUI/Asset/Lib/Bin/QRPBridge.dylib \
 -std=c++17 -Wall -pedantic -Wextra -Wno-unused-parameter \
 -arch x86_64 -arch arm64 \
+-mmacosx-version-min=10.12 \
 -dynamiclib -lz \
 -Ofast -funroll-loops \
 -I./godot-headers -I../LibRaw -I../libjpeg-turbo 
