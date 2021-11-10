8fb7490691cb8696479ed9e17faf970673f02041
qdwang <wangqidong@gmail.com>
2021-11-10 14:08:00 +0800

    adding glibc 2.23 header for linux

--- Library/QRPBridge/build.linux.sh
+++ Library/QRPBridge/build.linux.sh
@@ -1,4 +1,5 @@
 g++ main.cpp ../LibRaw/lib/libraw_mod.linux.a ../libjpeg-turbo/lib/libturbojpeg.linux.a \
+-include glibc-fix-header/force_link_glibc_2.23.h \
 -o ../../GUI/Asset/Lib/Bin/QRPBridge.so \
 -Wall -pedantic -Wextra -Wno-unused-parameter \
 -fpic -shared \
