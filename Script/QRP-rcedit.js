fdacc1094ed2ce7ab7714935225868c9b56bdafa
qdwang <wangqidong@gmail.com>
2021-11-12 14:49:33 +0800

    fix rcedit in QRP action

--- Script/QRP-rcedit.js
+++ Script/QRP-rcedit.js
@@ -1,7 +1,7 @@
 // This is related to the "Windows Desktop" section in export_presets.cfg
 
 const rcedit = require('rcedit')
-rcedit("../GUI/Build/win/QuickRawPicker-v0.2.1-win-x64.exe", {
+rcedit(`../GUI/Build/win/${process.argv[2]}`, {
   "icon": "../GUI/Asset/Texture/icon.ico",
   "version-string": {
     CompanyName: "qdwang",
